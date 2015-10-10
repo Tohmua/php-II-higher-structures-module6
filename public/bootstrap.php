4df05f301d3e52a4da5f7b0de1f20e3354f111b0
Oliver Wethey <oliver.musgrove-wethey@mayden.co.uk>
2015-10-10 17:22:33 +0100

    Move the code that adds the DB object into the service locator into the bootstrap file

--- public/bootstrap.php
+++ public/bootstrap.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\DependencyInjection\ServiceLocator;
+use App\Database\DatabaseConnector;
+use App\Database\DatabaseCreationException;
+use App\Database\PDODatabase;
 
 // Change the directory to the root of the application not the public folder.
 // Should make requiring easier
@@ -14,4 +17,19 @@ if (file_exists('src/App/Config/application.config.php')) {
     $serviceLocator->set('config', require 'src/App/Config/application.config.php');
 } else {
     echo 'Missing App/Config/application.config.php file';
+}
+
+try {
+    $dbConfig = $serviceLocator->get('config')['database'];
+
+    $serviceLocator->set(
+        'database',
+        new DatabaseConnector(
+            new PDODatabase(),
+            $dbConfig
+        )
+    );
+} catch (DatabaseCreationException $e) {
+    echo $e->getMessage();
+    exit();
 }
--- public/index.php
+++ public/index.php
@@ -2,25 +2,6 @@
 
 namespace DbExample;
 
-use App\Database\DatabaseConnector;
-use App\Database\DatabaseCreationException;
-use App\Database\PDODatabase;
-
 require_once __DIR__ . '/bootstrap.php';
 
-try {
-    $dbConfig = $serviceLocator->get('config')['database'];
 
-    $serviceLocator->set(
-        'database',
-        new DatabaseConnector(
-            new PDODatabase(),
-            $dbConfig
-        )
-    );
-} catch (DatabaseCreationException $e) {
-    echo $e->getMessage();
-    exit();
-}
-
-var_dump($dbConfig);
